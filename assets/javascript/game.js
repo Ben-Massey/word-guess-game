c70dfa389c197016bcd8bcb1cfc8e1278dee0559
Ben-Massey <pojoben@gmail.com>
2018-09-06 10:47:02 -0500

    did a lot since last commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,116 @@
-var words = ['cayde', 'ikora', 'zavala', 'xol', 'crucible'];
-var randomNum = math.floor(math.random()*words.length);
-var randomWord = words[randNum];
-var underscores = [];
-var rightLetter = [];
-var wrongLetter = [];
+var doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+                  
+var wordBank =['cayde','zavala','ikora', 'xol','crucible','gaurdian','ghost', 'shaxx', 'fallen', 'vex', 'cabal', 'hive', 'scorn', 'taken'];
+var choosenWord = "";
+var lettersInWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses =[];
+var wrongLetters = [];
 
-var underscoregenerator = () => {
-    for (let i = 0; i < randomWord.length; i++) {
-        underscores.push('_'); 
-    }
-    return underscores;
+var winCount = 0;
+var loseCount = 0;
+var guessesLeft = 9;
+var rightGuessCounter = 0;
+
+function reset()
+{
+	choosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	lettersInWord = choosenWord.split('');
+	numBlanks = lettersInWord.length;
+	
+
+	letterGuessed = 0;
+	rightGuessCounter = 0;
+	guessesLeft = 9;
+	wrongLetters =[];
+	blanksAndSuccesses =[];
+	doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	test=false;
+	startGame();
 }
-document.addEventListener('keypress', (event) => {
-    var keyword = String.fromCharCode(event.KeyCode);
-    if(randomWord.indexOf(keyword) > -1) {
-        rightLetter.push(keyword);
-    }
-    else {
-        wrongLetter.push(keyword);
-    }
-    underscores[randomWord.indexOf(keyword)] = keyword;
+function startGame()
+{
+	choosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	lettersInWord = choosenWord.split('');
+	numBlanks = lettersInWord.length;
+	rightGuessCounter = 0;
+	guessesLeft = 9;
+	wrongLetters =[];
+	blanksAndSuccesses =[];
+	doubleWord = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-    if(underscores.join('') == randomWord) {
-        alert("you win!")
-    }
-});
+	for(var i = 0; i< numBlanks; i++)
+	{
+		blanksAndSuccesses.push('_');
+		document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
+	}
+
+	document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
+	document.getElementById('numGuesses').innerHTML = guessesLeft;
+	document.getElementById('winCounter').innerHTML = winCount;
+	document.getElementById('lossCounter').innerHTML = loseCount;
+	document.getElementById('wrongGuesses').innerHTML = wrongLetters;
+}
+
+function compareLetters(userKey)
+{
+				if(choosenWord.indexOf(userKey) > -1)
+				{
+					for(var i = 0; i < numBlanks; i++)
+					{
+						if(lettersInWord[i] === userKey)
+						{
+							rightGuessCounter++;
+							blanksAndSuccesses[i] = userKey;
+							document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
+						}	
+					}
+				}
+				else
+				{
+					wrongLetters.push(userKey);
+					guessesLeft--;
+					document.getElementById('numGuesses').innerHTML = guessesLeft;
+					document.getElementById('wrongGuesses').innerHTML = wrongLetters;
+					console.log('Wrong Letters = ' + wrongLetters);
+					console.log('Guesses left are ' + guessesLeft);
+				}
+			
+			
+		
+}
+function winLose()
+{
+	if(rightGuessCounter === numBlanks)
+	{ 
+		winCount++;
+		document.getElementById('winCounter').innerHTML = winCount;
+		alert('haha! Is that what victory smells like? - Shaxx');
+		reset();
+	}
+	else if(guessesLeft === 0)
+	{
+		loseCount++;
+		document.getElementById('lossCounter').innerHTML = loseCount;
+		alert('Let the sting from this defeat fuel your fire! - Shaxx');
+		reset();
+	}
+}
+
+startGame();
+
+document.onkeyup = function(event)
+{
+	test = true;
+	var letterGuessed = event.key;
+	for(var i = 0; i < doubleWord.length; i++)
+	{	
+		if(letterGuessed === doubleWord[i] && test === true)
+		{
+			var splicedWord = doubleWord.splice(i,1);
+        compareLetters(letterGuessed);
+			winLose();
+		}
+	}		
+		
+}
